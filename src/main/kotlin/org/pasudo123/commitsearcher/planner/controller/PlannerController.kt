33feebdd0bc8beba2615cabca8e65d09129c074c
PARK SUNG DONG <pasudo222@gmail.com>
2021-02-13 21:45:33 +0900

    [add] controller & service layer added (#3)
    * [add] db initialize
    
    * [add] planner initializer & test code
    
    * [add] frontend directory
    
    * [add] planner controller & service classes

---
+++ src/main/kotlin/org/pasudo123/commitsearcher/planner/controller/PlannerController.kt
@@ -0,0 +1,33 @@
+package org.pasudo123.commitsearcher.planner.controller
+
+import org.pasudo123.commitsearcher.planner.dto.PlannerCreateDto
+import org.pasudo123.commitsearcher.planner.dto.PlannerResponseDto
+import org.pasudo123.commitsearcher.planner.service.PlannerService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/planners")
+class PlannerController(
+    private val plannerService: PlannerService
+) {
+
+    @PostMapping
+    fun createPlanner(@RequestBody plannerRequestDto: PlannerCreateDto.RequestDto): ResponseEntity<PlannerCreateDto.ResponseDto> {
+        return ResponseEntity.ok(plannerService.createPlanner(plannerRequestDto))
+    }
+
+    @GetMapping
+    fun findAll(): ResponseEntity<List<PlannerResponseDto>> {
+        return ResponseEntity.ok(plannerService.findAll())
+    }
+
+    @GetMapping("{id}")
+    fun findOneById(@PathVariable("id") id: Long): ResponseEntity<PlannerResponseDto> {
+        return ResponseEntity.ok(plannerService.findOneById(id))
+    }
+
+}
---
+++ src/main/kotlin/org/pasudo123/commitsearcher/planner/dto/PlannerResponseDto.kt
@@ -0,0 +1,8 @@
+package org.pasudo123.commitsearcher.planner.dto
+
+data class PlannerResponseDto(
+    var title: String = "",
+    var description: String = "",
+    var createdDate: String = "",
+    var updateDate: String = ""
+)
